37c86317fc20fd8c7adf24669d9e30a1c36ee59e
AsharSheikh <40037993+AsharSheikh@users.noreply.github.com>
2021-01-22 21:46:53 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from flask import Flask
 from selenium import webdriver
 import os
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def hello_world():
    chrome_options = webdriver.ChromeOptions()
+   
    chrome_options.add_argument("--headless")
    chrome_options.add_argument("--disable-dev-shm-usage")
    chrome_options.add_argument("--no-sandbox")
+   chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+   
+   driver.set_window_position(0, 0)
+   driver.set_page_load_timeout(20)
+   driver.implicitly_wait(20)
    driver.get("https://medium.com")
+   driver.set_window_size(1920, 1080)
+
+   time.sleep(3)
+
+   png = driver.get_screenshot_as_png()
    print("Finished!")
    driver.quit()
-   return '<h1>Hello World</h1>'
+   
+   return Response(png, mimetype="image/png")
