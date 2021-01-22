6258f4dcc390c9ff75f192c40f8df878b4475aef
AsharSheikh <40037993+AsharSheikh@users.noreply.github.com>
2021-01-22 23:03:39 +0500

    Update main.py
    Changed the link to SECP website

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 from selenium import webdriver
 import os
 import time
@@ -12,15 +12,21 @@ def hello_world():
    
    chrome_options.add_argument("--headless")
    chrome_options.add_argument("--disable-dev-shm-usage")
-   chrome_options.add_argument("--no-sandbox")
+   chrome_options.add_argument("--no-sandbox")C
    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-
+   
+   CUIN = request.args.get("cuin", "")
+   
+   if not CUIN:
+        return "Example: <a href='http://secp-scraping.herokuapp.com/?cuin=0155624'>" \
+               "http://secp-scraping.herokuapp.com/?cuin=0155624</a>"
+      
    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
    
    driver.set_window_position(0, 0)
    driver.set_page_load_timeout(20)
    driver.implicitly_wait(20)
-   driver.get("https://medium.com")
+   driver.get("https://www.secp.gov.pk/company-name-search/company-details/?companyCUIN=" + CUIN)
    driver.set_window_size(1920, 1080)
 
    time.sleep(3)
@@ -32,4 +38,4 @@ def hello_world():
   
    driver.quit()
    
-   return src_base64
+   return "<img src='data:image/png;base64," + src_base64 + "'/>"
