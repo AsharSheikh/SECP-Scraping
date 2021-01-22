44c6408da51398dd3a1d111eea053eab04b3c28e
AsharSheikh <40037993+AsharSheikh@users.noreply.github.com>
2021-01-22 19:55:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 @app.route('/')
-def app():
+def index():
    chrome_options = webdriver.ChromeOptions()
    chrome_options.add_argument("--headless")
    chrome_options.add_argument("--disable-dev-shm-usage")
