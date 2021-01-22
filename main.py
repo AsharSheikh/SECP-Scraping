2578a079636d6d7fd870592dd37a2a9fce359f99
AsharSheikh <40037993+AsharSheikh@users.noreply.github.com>
2021-01-22 23:07:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def hello_world():
    
    chrome_options.add_argument("--headless")
    chrome_options.add_argument("--disable-dev-shm-usage")
-   chrome_options.add_argument("--no-sandbox")C
+   chrome_options.add_argument("--no-sandbox")
    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
    
    CUIN = request.args.get("cuin", "")
